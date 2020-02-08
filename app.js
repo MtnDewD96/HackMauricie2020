f10d01578a287fa4ef8f83553ea1f648c6787683
saynnn <39345951+saynnn@users.noreply.github.com>
2020-02-08 14:26:36 -0500

    test

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const testRouter = require('./routes/test');
 
 const app = express();
 
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'vendor/node_modules')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/test', testRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
